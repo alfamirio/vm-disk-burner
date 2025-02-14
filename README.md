a794bcc3188266739a2a409de6690b8638e567d6
alfamirio <alfamirio@gmail.com>
2025-02-14 06:43:04 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # vm-disk-burner
-Small bash script to burn a vmdk, vdi o vhd(x) to a usb drive
+Small bash script to burn a vmdk, vdi o vhd(x) to a usb drive/nvme-usb drive.
+It prompts for the disk location and target unit to burnt the image.
+
+# purpose
+It is designed for fast v2p (virtual to physical).
+
+# how it works
+It used qemu-utils ndb to mount the vm disk image and later uses dd to burn the mounted image to a usb unit.
+This way avoids the step of converting the compressed vm disk image to an uncompressed .img/.raw that can be burn
+with Gnome-Disk or Rufus. So mostly about saving the unsparce/inflating step of using a raw image. 
+
+# warning
+Take extreme care when selecting the target disk, as it will dd the device.
+NO WARRANTY. USE WITH CARE.
---
+++ vm-disk-burner.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+function is_a_valid_vm_disk_image () {
+    case "$1" in
+    *.vmdk | *.vdi | *.vhd | *.vhdx )
+            # it's valid
+            echo true;
+            ;;
+    *)
+            # it's not
+            echo false;
+            ;;
+    esac
+}
+
+function is_a_valid_disk () {
+    existDisk=$(fdisk -l $1)
+    case "$existDisk" in
+    "" )
+            # it's not valid
+            echo false;
+            ;;
+    *)
+            # it's
+            echo true;
+            ;;
+    esac
+}
+
+echo -e "vm-disk-burner 0.1 beta"
+echo -e "NO WARRANTY. USE WITH CARE.\n"
+
+sepLine=$(echo -$___{1..40} | tr -d ' ')
+path=""
+validDiskImage=false
+
+while [ "$validDiskImage" == false ];
+do
+    read -e -p "path to disk image (vmdk/vdi/vhd[x])): " path
+
+    if [ -f $path ];
+    then
+        valid_disk_image=$(is_a_valid_vm_disk_image $path)
+        if [ "$valid_disk_image" == true ];
+        then
+            echo -e "\nDisk image exists"
+            ls -lh $path
+            echo -e "\n"
+            validDiskImage=true
+        else
+            echo "File is not a disk image"
+        fi
+    else
+        echo "File does not exist"
+    fi
+done
+
+sleep 2
+echo -e "List of disks"
+echo "$sepLine"
+sudo fdisk -l
+echo -e "$sepLine\n"
+
+target=""
+validTargetDisk=false
+
+while [ "$validTargetDisk" == false ];
+do
+    read -e -p "target disk: " target
+
+    valid_target_disk=$(is_a_valid_disk $target)
+    if [ "$valid_target_disk" == true ];
+    then
+        echo -e "\nValid target disk"
+        fdisk -l $target
+        echo -e "\n"
+        validTargetDisk=true
+    else
+        echo "Target is not a disk"
+    fi
+
+done
+
+
+echo -e "mounting disk image ..."
+# we enable ndb to read disk image, max 8 partitions, 2 ndb devices
+modprobe nbd max_part=8 nbds_max=2
+
+# mount disk image with nbd
+filename=$(basename -- "$path")
+extension="${filename##*.}"
+filename="${filename%.*}"
+qemu-nbd -c /dev/nbd0 -f $extension $path
+echo -e "mounting disk image done"
+
+
+# burning the data
+echo -e "burning disk image to disk ..."
+echo -e "\nburning $path to $target in progress"
+dd if=/dev/nbd0 of=$target bs=64M status=progress
+echo -e "burning disk image to disk done"
+
+
+# disable ndb
+echo -e "dismounting disk image ..."
+qemu-nbd --disconnect /dev/nbd0
+rmmod nbd
+echo -e "dismounting disk image done"
